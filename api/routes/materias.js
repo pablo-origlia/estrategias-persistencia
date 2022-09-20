6f12abe4954e1c5e200e7b014f5e9635c1288a47
pablo-origlia <pablo.origlia@estudiantes.unahur.edu.ar>
2022-09-20 18:53:05 -0300

    Se agregan metodos PUT y DELETE en materia.

--- api/routes/materias.js
+++ api/routes/materias.js
@@ -21,9 +21,7 @@ router.post("/", (req, res) => {
     .then((materia) => res.status(201).send({ id: materia.id }))
     .catch((error) => {
       if (error == "SequelizeUniqueConstraintError: Validation error") {
-        res
-          .status(400)
-          .send("Bad request: existe otra materia con el mismo nombre");
+        res.status(400).send("Bad request: existe otra materia con el mismo nombre");
       } else {
         console.log(`Error al intentar insertar en la base de datos: ${error}`);
         res.sendStatus(500);
@@ -44,6 +42,39 @@ const findMateria = (id, { onSuccess, onNotFound, onError }) => {
 router.get("/:id", (req, res) => {
   findMateria(req.params.id, {
     onSuccess: (materia) => res.send(materia),
+    onNotFound: () => res.sendStatus(404),
+    onError: () => res.sendStatus(500),
+  });
+});
+
+router.put("/:id", (req, res) => {
+  const onSuccess = (materia) =>
+    materia
+      .update({ nombre: req.body.nombre }, { fields: ["nombre"] })
+      .then(() => res.sendStatus(200))
+      .catch((error) => {
+        if (error == "SequelizeUniqueConstraintError: Validation error") {
+          res.status(400).send("Bad request: existe otra materia con el mismo nombre");
+        } else {
+          console.log(`Error al intentar actualizar la base de datos: ${error}`);
+          res.sendStatus(500);
+        }
+      });
+  findMateria(req.params.id, {
+    onSuccess,
+    onNotFound: () => res.sendStatus(404),
+    onError: () => res.sendStatus(500),
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  const onSuccess = (materia) =>
+    materia
+      .destroy()
+      .then(() => res.sendStatus(200))
+      .catch(() => res.sendStatus(500));
+  findMateria(req.params.id, {
+    onSuccess,
     onNotFound: () => res.sendStatus(404),
     onError: () => res.sendStatus(500),
   });
