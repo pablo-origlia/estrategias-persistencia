f70223d122d22eb8a7dd03ad5f25d79340bb9a2f
pablo-origlia <pablo.origlia@estudiantes.unahur.edu.ar>
2022-10-10 15:21:44 -0300

    Se agregan test básicos para los metodos GET

--- api/tests/routes.test.js
+++ api/tests/routes.test.js
@@ -1,25 +1,36 @@
 var request = require("supertest");
 var app = require("../app");
 
-describe("Good Home Routes", function () {
-  test("responds to /car", async () => {
+describe("Test básicos de los metodos GET", function () {
+  test("respuesta a /car", async () => {
     const res = await request(app).get("/car");
     expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
     expect(res.statusCode).toBe(200);
-    //expect(res.text).toEqual("[]");
+    expect(res.body).toMatchObject([
+      { id: 1, nombre: "Ing Informática" },
+      { id: 2, nombre: "Ing Electrónica" },
+      { id: 3, nombre: "Ing Mecánica" },
+      { id: 4, nombre: "Ing Robotica" },
+      { id: 5, nombre: "Ing Civil" },
+    ]);
   });
 
-  test("responds to /mat", async () => {
+  test("respuesta a /mat", async () => {
     const res = await request(app).get("/mat");
     expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
     expect(res.statusCode).toBe(200);
-    //expect(res.text).toEqual("[]");
+    expect(res.body).toMatchObject([{ id: 1, nombre: "Programación 1", id_carrera: null, "Carrera-Relacionada": null }]);
   });
 
-  test("responds to /alu", async () => {
+  test("respuesta a /alu", async () => {
     const res = await request(app).get("/alu");
     expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
     expect(res.statusCode).toBe(200);
-    //expect(res.text).toEqual("[]");
+    expect(res.body).toMatchObject([
+      { id: 2, apellido: "Origlia", nombre: "Pablo", dni: 29904177, id_carrera: 1, "Carrera-Relacionada": { id: 1, nombre: "Ing Informática" } },
+      { id: 3, apellido: "Perez", nombre: "Juan", dni: 32456234, id_carrera: 1, "Carrera-Relacionada": { id: 1, nombre: "Ing Informática" } },
+      { id: 4, apellido: "Gomez", nombre: "Roberto", dni: 45776345, id_carrera: 3, "Carrera-Relacionada": { id: 3, nombre: "Ing Mecánica" } },
+      { id: 5, apellido: "Golon", nombre: "Ernesto", dni: 45776345, id_carrera: 10, "Carrera-Relacionada": null },
+    ]);
   });
 });
